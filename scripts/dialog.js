9fe6f1d5f69a2e5ce16fda6487150fa8a53814b1
SonicHsu <soniccz@gmail.com>
2025-03-31 06:57:30 +0800

    完成刪除事件的功能

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -8,7 +8,7 @@ export function initDialog(name) {
     function close() {
         dialogElement.classList.add("dialog--closing");
 
-        waitUntilAnimationsFinish(dialogElement)
+        return waitUntilAnimationsFinish(dialogElement)
         .then(() => {
             dialogElement.classList.remove("dialog--closing");
             dialogElement.close();
@@ -41,7 +41,7 @@ export function initDialog(name) {
             dialogElement.showModal();
         },
         close() {
-            close();
+            return close();
         }
     };
 }
